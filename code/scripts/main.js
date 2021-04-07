95b2aa269f5264c7a3dfe181664018b16547d2a4
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2021-04-07 06:24:42 +0200

    idk

--- code/scripts/main.js
+++ code/scripts/main.js
@@ -1,2 +1,29 @@
 
 console.log("Hello World");
+
+
+let leftView = 1600, rightView = 1900;
+
+class TimelineComponent extends HTMLElement {
+    constructor() {
+        super();
+
+    }
+
+    connectedCallback() {
+        for (let i = 0; i < 60; i++) {
+            let e = document.createElement('div');
+            
+        }
+    }
+}
+customElements.define('time-line', TimelineComponent);
+
+class EpochComponent extends HTMLElement {
+    constructor() {
+        super();
+
+    }
+}
+
+customElements.define('epoch-comp', EpochComponent);
