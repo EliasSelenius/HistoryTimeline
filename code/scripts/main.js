eaa408b983fdc53ffef58580245147f7c762eebb
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2021-04-07 19:05:11 +0200

    working zooming and paning of timeline

--- code/scripts/main.js
+++ code/scripts/main.js
@@ -1,50 +1,83 @@
 
 console.log("Hello World");
 
-const lerp = (x, y, t) => (1 - t) * x + t * y;
-
 
 let leftView = 1600, rightView = 1900;
 
 class TimelineComponent extends HTMLElement {
     constructor() {
         super();
-        this.addEventListener('scroll', (el, e) => {
+        /*this.addEventListener('onscroll', (el, e) => {
             console.log("sc");
-        });
+        });*/
+
+        this.onwheel = e => {
+            this.zoomTimeline(e.deltaY / 10);
+        }
+
+        this.onmousedown = e => {
+            this._draging = true;
+        }
+
+        this.onmouseup = e => this._draging = false;
+        this.onmouseleave = e => this._draging = false;
+        
+
+        this.onmousemove = e => {
+            if (this._draging) {
+                this.panTimeline(e.movementX);
+            }
+        }
     }
 
-    updateView(min, max) {
-        leftView = min;
-        rightView = max;
+    zoomTimeline(zoomlevel) {
+        leftView -= zoomlevel;
+        rightView += zoomlevel;
+        this.updateView();
+    }
 
-        
+    panTimeline(pan) {
+
+        const scale = (rightView - leftView) / 1000;
+
+        leftView -= pan * scale;
+        rightView -= pan * scale;
+        this.updateView();
+    }
+
+    updateView() {
+        if (leftView > rightView) {
+            leftView = rightView = (leftView + rightView) / 2;
+            leftView -= 1; rightView += 1;
+            return;
+        }
+
+        const markers = this.getElementsByTagName('time-line-marker');
+        for (let i = 0; i < markers.length; i++) {
+            markers.item(i).updateLocation();
+        }
     }
 
     connectedCallback() {
-        //this.updateView(1500, 1750);
-
-
+        this.updateView(1500, 1750);
 
         fetch('../data/events.json').then(x => x.json()).then(x => {
-            
-
             for (let i = 0; i < x.length; i++) {
                 const event = x[i];
-
-                var marker = document.createElement('time-line-marker');
-                marker.year = event.year;
-                marker.innerText = event.name;
-                this.appendChild(marker);
-        
+                this.createMarker(event.year, event.name);
             }
-
-        })
+        });
 
     }
 
 
-    
+    createMarker(year, name) {
+        var marker = document.createElement('time-line-marker');
+        marker.year = year;
+        marker.innerText = name;
+        this.appendChild(marker);
+
+    }
 
 
 }
